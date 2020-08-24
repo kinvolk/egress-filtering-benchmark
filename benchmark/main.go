cd1cb164b1bbffc0c91e513c38a37b9b256d47ac
Mauricio Vásquez <mauricio@kinvolk.io>
2020-08-24 17:06:44 -0500

    Add ipset filter

--- benchmark/main.go
+++ benchmark/main.go
@@ -9,6 +9,7 @@ import (
 	"net"
 
 	"github.com/kinvolk/k8s-egress-filtering-benchmark/pkg/filters/bpf"
+	"github.com/kinvolk/k8s-egress-filtering-benchmark/pkg/filters/ipset"
 	"github.com/kinvolk/k8s-egress-filtering-benchmark/pkg/filters/iptables"
 	"github.com/kinvolk/k8s-egress-filtering-benchmark/pkg/ipnetsgenerator"
 )
@@ -31,7 +32,7 @@ func init() {
 	flag.IntVar(&countParam, "count", 0, "Number of entries to generate")
 	flag.StringVar(&ipnetsParam, "ipnets", "", "List of ipnets and their weigth to generate (ex. 24:0.7,16:0.1)")
 	flag.Int64Var(&seed, "seed", 0, "Seed to use for the random generator")
-	flag.StringVar(&filterType, "filter", "", "Type of filter to use (bpf, iptables)")
+	flag.StringVar(&filterType, "filter", "", "Type of filter to use (bpf, iptables, ipset)")
 }
 
 func main() {
@@ -51,6 +52,8 @@ func main() {
 		filter = bpf.New()
 	case "iptables":
 		filter = iptables.New()
+	case "ipset":
+		filter = ipset.New()
 	default:
 		fmt.Printf("%q is not a valid filter type", filterType)
 		os.Exit(1)
