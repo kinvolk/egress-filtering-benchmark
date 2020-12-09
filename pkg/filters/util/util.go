d8d637d65a154db57955896eaadb6c324d99a8d2
Imran Pochi <imran@kinvolk.io>
2020-12-09 12:44:34 +0530

    refactor constants
    Signed-off-by: Imran Pochi <imran@kinvolk.io>

--- pkg/filters/util/util.go
+++ pkg/filters/util/util.go
@@ -5,6 +5,19 @@ import (
 	"text/template"
 )
 
+const (
+	// This number is restricted by the number of entries that can be present in an ipset
+	// While that number could be `1048576`, the actual number is far less because of the
+	// maximum size that can be POST'ed to the Kubernetes API server before getting error
+	// `etcdserver: Request entity too large`.
+	// This number can certainly be improved upon which would result in lesser number of
+	// GlobalNetworkSet batch thereby resulting in quicker turnaround time of applying the
+	// manifests. However the expected improvement is small.
+	RulesPerManifest = 50000
+	// Timout in milliseconds.
+	Timeout = 600000
+)
+
 func RenderTemplate(tmpl string, obj interface{}) (string, error) {
 	t, err := template.New("render").Parse(tmpl)
 	if err != nil {
